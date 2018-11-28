8a17ff05ac367fd17ecd18f8869b8e8a7a213b8e
Zhanliang Liu <zhanliangliu@qq.com>
2018-11-28 17:52:18 +0800

    playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+//import UIKit
+
+var str = "Hello, playground"
+
+print("Hello World")
+
+var age:Int = 90
+var game="老鹰抓小鸡"
+var age1 = 30
+
+print(age1)
+print("game的值为:\(game)")
+
+let minValue = UInt8.min // minValue为 0
+let maxValue = UInt8.max // maxValue为 255
+
+print("maxValue of UInt8 is \(maxValue)")
+
+var game1 = (1, 2 , "九阴真经")
+print("game1元组的值为：\(game1)")
+print("game1元组的名称元素为：\(game1.2)")
